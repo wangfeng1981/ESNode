453da01a2297228608445cab9b0e8fe1dbe18933
wangfeng1981 <jfwf@yeah.net>
2013-03-30 20:32:29 +0800

    Initial Commit

---
+++ ESNode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ESNode
+//
+//  Created by Wang Feng on 13-3-30.
+//  Copyright (c) 2013年 jfwf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
